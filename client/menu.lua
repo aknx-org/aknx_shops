f33bbbbf4562211e4b9ac8d151fb2a61f7aa0b88
NirosTen <57236041+NirosTen@users.noreply.github.com>
2021-06-27 20:17:02 +0200

    Update menu.lua

--- client/menu.lua
+++ client/menu.lua
@@ -23,32 +23,3 @@ function openSuperette()
         end)
     end
 end
-
-
-Citizen.CreateThread(function()
-    while true do
-        local pNear = false
-
-        for k,v in pairs(config.zones) do 
-            local distance = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), v)
-            local marker = config.marker
-
-            if distance <= 2.0 then
-                pNear = true
-                if marker.haveMarker then
-                    DrawMarker(marker.ID, v, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, marker.Size, marker.Size, marker.Size, marker.Color[1], marker.Color[2], marker.Color[3], marker.Alpha, 0, 1, 2, 0, nil, nil, 0)
-                end
-                ESX.ShowHelpNotification("Appuyer sur [E] pour ouvrir la Superette")
-                if IsControlJustReleased(0, 38) then
-                    openSuperette()
-                end
-            end
-        end
-
-        if pNear then
-            Wait(1)
-        else
-            Wait(250)
-        end
-    end
-end)

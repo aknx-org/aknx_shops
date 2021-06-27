c93ca60d170776ec016dc765de4b964ac7db2bf8
NirosTen <57236041+NirosTen@users.noreply.github.com>
2021-06-27 21:06:19 +0200

    Add traduction

---
+++ traduction.lua
@@ -0,0 +1,6 @@
+TRADUCTION = {
+    PRESS_MENU = "Appuyer sur [E] pour ouvrir la Superette",
+    ITEM_BOUGHT = "Vous avez acheté",
+    PLAYER_CANNOT_HOLD = "Vous n'avez pas assez de place dans votre inventaire !",
+    NOT_ENOUGH = "Vous n,'avez pas assez d'argent, il vous manque "
+}

6c2945fac27a937f1d62c6bc6309b7cf0f11e210
NirosTen <57236041+NirosTen@users.noreply.github.com>
2021-06-27 21:16:35 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,20 +1,33 @@
+ESX = nil
+TriggerEvent(config.GetESX, function(obj) ESX = obj end)
+
 RegisterNetEvent("aknx:buyItem")
 AddEventHandler("aknx:buyItem", function(label, item, price, amount)
     local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 
     local totalPrice = price * amount
-
-    if xPlayer.getMoney() >= totalPrice then
-        if xPlayer.canCarryItem(item, amount) then
-            xPlayer.removeMoney(totalPrice)
-            xPlayer.addInventoryItem(item, amount)
-            TriggerClientEvent('esx:showNotification', _source, TRADUCTION.ITEM_BOUGHT.." "..amount.." "..item)
+    if not config.isLimit then
+        if xPlayer.getMoney() >= totalPrice then
+            if xPlayer.canCarryItem(item, amount) then
+                xPlayer.removeMoney(totalPrice)
+                xPlayer.addInventoryItem(item, amount)
+                TriggerClientEvent('esx:showNotification', _source, TRADUCTION.ITEM_BOUGHT.." "..amount.." "..item)
+            else
+                TriggerClientEvent('esx:showNotification', _source, TRADUCTION.PLAYER_CANNOT_HOLD)
+            end
         else
-            TriggerClientEvent('esx:showNotification', _source, TRADUCTION.PLAYER_CANNOT_HOLD)
+            local missingMoney = totalPrice - xPlayer.getMoney()
+            TriggerClientEvent('esx:showNotification', _source, TRADUCTION.NOT_ENOUGH.." "..missingMoney.."$")
         end
     else
-        local missingMoney = totalPrice - xPlayer.getMoney()
-        TriggerClientEvent('esx:showNotification', _source, TRADUCTION.PLAYER_CANNOT_HOLD.." "..missingMoney.."$")
+        if xPlayer.getMoney() >= totalPrice then
+            xPlayer.removeMoney(totalPrice)
+            xPlayer.addInventoryItem(item, amount)
+            TriggerClientEvent('esx:showNotification', _source, TRADUCTION.ITEM_BOUGHT.." "..amount.." "..label)
+        else
+            local missingMoney = totalPrice - xPlayer.getMoney()
+            TriggerClientEvent('esx:showNotification', _source, TRADUCTION.NOT_ENOUGH.." "..missingMoney.."$")
+        end
     end
 end)
